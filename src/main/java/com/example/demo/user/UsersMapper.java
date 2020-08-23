b24d132ddf4939c4888b65f6583ac255c373c6c6
Musab <b_musab@hotmail.com>
2020-08-23 15:48:37 +0300

    some changes

--- src/main/java/com/example/demo/user/UsersMapper.java
+++ src/main/java/com/example/demo/user/UsersMapper.java
@@ -1,10 +1,8 @@
 package com.example.demo.user;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @Mapper
